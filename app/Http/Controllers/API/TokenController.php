ceab6832877c6bdc0cb02f2f2a1a07ace5eda41d
Ingvar <ingvar.soloma@gmail.com>
2022-05-21 19:58:54 +0300

    api/token: generate and save to the cookie

---
+++ app/Http/Controllers/API/TokenController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Support\Facades\Cookie;
+
+class TokenController extends Controller
+{
+    public function getToken(): array
+    {
+        try {
+            $token = bin2hex(random_bytes(15));
+            $expires = time() + 60 * 40;
+            $path = '/api/users';
+            setcookie('registration_token', $token, $expires, $path);
+
+            $result["registration_token"] = $token;
+            $result["success"] = true;
+
+        } catch (Exception $e) {
+            $result = ["success" => false];
+        }
+        return $result;
+    }
+}
